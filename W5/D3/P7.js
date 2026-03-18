714aad756ed36d982675eecb523d986fc87c83d2
nithyatn28 <nithyatn28@gmail.com>
2026-03-18 12:25:48 +0530

    sequenstial vs parallel promise execution

---
+++ W5/D3/P7.js
@@ -0,0 +1,32 @@
+//sequenstial v/s parallel promise execution
+function taskA(){
+    return new Promise(function(resolve) {
+        setTimeout(function(){
+            resolve("Task A completed");
+        },1000);
+    });
+}
+function taskB(){
+    return new Promise(function(resolve) {
+        setTimeout(function(){
+            resolve("Task B completed");
+        },1000);
+    });
+}
+async function runSequential() {
+    console.time("Sequential") ;
+    const a = await taskA();  
+    const b = await taskB();  
+    console.timeEnd("Sequential");
+    console.log(a);
+    console.log(b);
+}
+await runSequential();
+async function runparallel() {
+    console.time("Parallel") ;
+    const result = await Promise.all([taskA(),taskB()]);  
+    console.timeEnd("Parallel");
+    console.log(result[0]);
+    console.log(result[1]);
+}
+runparallel();
