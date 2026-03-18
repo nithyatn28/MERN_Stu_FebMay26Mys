67fb3d4c62a22addcb14e72527122a9d1bd26f55
nithyatn28 <nithyatn28@gmail.com>
2026-03-18 10:41:25 +0530

    why use of async/await

---
+++ W5/D3/P2.js
@@ -0,0 +1,23 @@
+//why use async/await
+function getUser(){
+    return new Promise((resolve) => {
+        setTimeout(function() {
+            resolve({id:101,name:"karna"});
+        },1000);
+    });
+}
+function getOrders(userId) {
+    return new Promise((resolve) => {
+        setTimeout(function(){
+            resolve(["order-A","order-B"]);
+        },20000);
+    })
+    
+}
+async function showUserAndOrders(){
+    const user =  getUser();
+    console.log("User loaded",user.name);
+    const order = await getOrders(user.id);
+    console.log("Orders areloaded",order);
+}
+showUserAndOrders();
