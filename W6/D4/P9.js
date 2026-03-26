c03ada046a89607187dcad1583d47a2b1368104d
nithyatn28 <nithyatn28@gmail.com>
2026-03-26 17:32:18 +0530

    route parameters using manual path parsing

---
+++ W6/D4/P9.js
@@ -0,0 +1,26 @@
+//route parameters using manual path parsing
+//her we learnt to extract dynamic value from a URL path
+const http = require("http");
+const server = http.createServer(function(req,res){
+    if(req.method === "GET"&& req.url.startsWith("/users/")){
+        //split the path and pick the last section as
+        //route parameter 
+        const parts = req.url.split("/");
+        const userId = parts[2];
+        res.writeHead(200,{"Content-Type":"application/json"});
+        res.end(JSON.stringify({
+            route:"/users/:id",
+            userId:userId
+        }));
+        return;
+
+    }
+    //405:URL route is valid but the method is not found 
+    res.writeHead(404, { "content-type": "application/json" });
+    res.end(JSON.stringify({message: "Route not found." }));
+
+});
+server.listen(3001, function () {
+    console.log("server is runinng at http://localhost:3001");
+
+});
