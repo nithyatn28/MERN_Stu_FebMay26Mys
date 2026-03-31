7783acbfcc7ede91bd653f505e26202de4209ef4
nithyatn28 <nithyatn28@gmail.com>
2026-03-31 17:11:56 +0530

    custom-middleware

---
+++ W7/D2/P3.js
@@ -0,0 +1,26 @@
+//custom-middleware
+const express = require("express");
+
+const app = express();
+//custom middleware
+function checkAdminAccess(req,res,next){
+    if(req.query.role!=="admin"){
+        return res.status(403).json({
+            success:false,
+            message:"Admin access denied"
+        });
+    }
+    next();
+}
+app.get("/",function(req,res){
+    res.send("Home route")
+});
+app.get("/admin",checkAdminAccess,function(req,res){
+    res.json({
+            success:true,
+            message:"Admin route access"
+        })
+});
+app.listen(4000,function(){
+    console.log("Express sever running at http://localhost:4000");
+});
