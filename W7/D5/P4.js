ad6bfecb6a8aba1fdad8a90761e16ffd674ff517
nithyatn28 <nithyatn28@gmail.com>
2026-04-03 12:25:13 +0530

    Basics of RBAC

---
+++ W7/D5/P4.js
@@ -0,0 +1,14 @@
+//Basics of RBAC
+const routePermissions = {
+    "/admin":["admin"],
+    "/reports":["admin","manager"],
+    "/profile":["admin","manager","user"]
+};
+function canAccess(route,role){
+    const allowedRoles = routePermissions[route]||[];
+    return allowedRoles.includes(role);
+}
+console.log("User access to /admin:",canAccess("/admin","user"));
+console.log("admin access to /admin:",canAccess("/admin","admin"));
+console.log("manager access to /reports:",canAccess("/reports","manager"));
+console.log("User access to /profile:",canAccess("/profile","user"));
