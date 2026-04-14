a57a23e1d7b0a969921883828c3668a5ae6383df
nithyatn28 <nithyatn28@gmail.com>
2026-04-14 13:26:31 +0530

    Team project

---
+++ TeamProject/LMS/MERN_Week1_V1/courses.js
@@ -0,0 +1,6 @@
+const courses=[
+  {id:1,title:"Node.js Basics",lessons:["Intro","Modules","Async","Events"],category:"Programming"},
+  {id:2,title:"MongoDB",lessons:["", "Data Types", "Loops"],category:"Programming"},
+  {id: 3,title:"Web Technology",lessons:["Intro","Form & Input Fields", "Layouts", "FlexBox", "Grid"],category:"Web Development"}
+];
+module.exports = courses;
